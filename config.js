ff7ead5b5eaa11e6ef7d90f5a24be949cd9d7677
yangguang liang <37032154+Soleil0707@users.noreply.github.com>
2026-02-09 14:41:19 +0000

    bless opt

--- config.js
+++ config.js
@@ -4,20 +4,18 @@ var config = {
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
         "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的姑娘",  // 同上...
+        "心爱的小菠萝",  // 同上...
         "今天是你的生日",
         "这是我们在一起的",
         "第三个生日！",
-        "这两年来，我见过你",
-        "奇怪的样子",
-        "可爱的样子",
-        "生气的样子",
-        "都是我喜欢的样子",
-        "对了，礼物收全了吗？",
-        "今年要吃好好的",
-        "喂饱饱的",
-        "然后才有力气干活",
-        "和想我！",
+        "在我们无法预料的未来里",
+        "希望你",
+        "永远做最真实的自己",
+        "永远熠熠生辉",
+        "希望未来",
+        "你每个有意义的时候",
+        "我都能陪着你",
+        "很开心你能出现在我的生命里",
         "生日快乐~~",
     ],
     /**
@@ -30,22 +28,22 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "送给我": "./imgs/songxinxin.jpeg",
-        "心爱的姑娘": "./imgs/nothing.jpg",
-        "今天是你的生日": "./imgs/shengri.jpeg",
-        "这是我们在一起的": "./imgs/zaiyiqi.png",
-        "第三个生日！": "./imgs/nothing.jpg",
-        "这两年来，我见过你": "./imgs/yangzi.jpeg",
-        "奇怪的样子": "./imgs/nothing.jpg",
-        "可爱的样子": "./imgs/nothing.jpg",
-        "生气的样子": "./imgs/nothing.jpg",
-        "都是我喜欢的样子": "./imgs/nothing.jpg",
-        "对了，礼物收全了吗？": "./imgs/chuo.gif",
-        "今年要吃好好的": "./imgs/chihaodian.jpeg",
-        "喂饱饱的": "./imgs/weibao.jpeg",
-        "然后才有力气干活": "./imgs/nothing.jpg",
-        "和想我！": "./imgs/nothing.jpg",
-        "生日快乐~~": "./imgs/nothing.jpg"
+        // "送给我": "./imgs/songxinxin.jpeg",
+        // "心爱的姑娘": "./imgs/nothing.jpg",
+        // "今天是你的生日": "./imgs/shengri.jpeg",
+        // "这是我们在一起的": "./imgs/zaiyiqi.png",
+        // "第三个生日！": "./imgs/nothing.jpg",
+        // "这两年来，我见过你": "./imgs/yangzi.jpeg",
+        // "奇怪的样子": "./imgs/nothing.jpg",
+        // "可爱的样子": "./imgs/nothing.jpg",
+        // "生气的样子": "./imgs/nothing.jpg",
+        // "都是我喜欢的样子": "./imgs/nothing.jpg",
+        // "对了，礼物收全了吗？": "./imgs/chuo.gif",
+        // "今年要吃好好的": "./imgs/chihaodian.jpeg",
+        // "喂饱饱的": "./imgs/weibao.jpeg",
+        // "然后才有力气干活": "./imgs/nothing.jpg",
+        // "和想我！": "./imgs/nothing.jpg",
+        // "生日快乐~~": "./imgs/nothing.jpg"
     },
     
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
@@ -61,5 +59,5 @@ var config = {
     },
 
     // 结束语
-    loveText: '爱你的X先生~'
+    loveText: '爱你的小阳光~'
 };
